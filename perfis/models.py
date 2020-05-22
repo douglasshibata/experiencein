4874a4777fc1b531498bd06a71d3508a5a41776d
Shibata18 <douglas.shibata11@gmail.com>
2020-05-22 18:14:02 -0300

    primeiro passos com persistência

--- perfis/models.py
+++ perfis/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 # Create your models here.
-
+class Perfil(models.Model):
+    nome = models.CharField(max_length=255, null=False)
+    email = models.CharField(max_length=255, null=False)     
+    telefone = models.CharField(max_length=15, null=False)
+    nome_empresa = models.CharField(max_length=255, null=False)
--- perfis/views.py
+++ perfis/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def exibir(request,perfil_id):
-    print('ID do perfil recebido: {}'.format(perfil_id))
-    return render(request,'perfil.html')
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request,'perfil.html',{'perfil':perfil})

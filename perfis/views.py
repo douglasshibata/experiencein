6d14d9f1a980e39a0dff4de780f68df6a7b7132f
Shibata18 <douglas.shibata11@gmail.com>
2020-05-25 15:44:24 -0300

    recebendo convites

--- perfis/views.py
+++ perfis/views.py
@@ -4,11 +4,11 @@ from perfis.models import Perfil
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html',{'perfis':Perfil.objects.all()})
+    return render(request, 'index.html', {'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
 def exibir(request,perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
-    return render(request,'perfil.html',{'perfil':perfil})
+    return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request)})
 
 def convidar(request, perfil_id):
     perfil_a_convidar = Perfil.objects.get(id=perfil_id)

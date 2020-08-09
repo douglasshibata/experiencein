ee8efed24fe054b9841e66a5819a446b0566df9b
Shibata18 <douglas.shibata11@gmail.com>
2020-08-09 12:17:03 -0300

    Django com mysql

--- experiencein/settings.py
+++ experiencein/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'experiencein.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'experiencein',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost', 
+        'PORT': '3306'
     }
 }
 

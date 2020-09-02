dabeb89599ebc7b464cc4d31e5ea11dcb169c8f5
Shibata18 <douglas.shibata11@gmail.com>
2020-09-02 18:48:27 -0300

    Mudando o banco de dados

--- experiencein/settings.py
+++ experiencein/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'experiencein.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+""" 
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.mysql', 
@@ -84,7 +84,14 @@ DATABASES = {
         'HOST': 'localhost', 
         'PORT': '3306'
     }
+} """
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
 
 
 # Password validation

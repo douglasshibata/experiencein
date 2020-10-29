39782747e07b979a21033d03f5ae9b777854d3be
Douglas Shibata <douglas.shibata11@gmail.com>
2020-10-29 14:45:52 -0300

    Adicionando os endpoints da api

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from perfis import views
+from rest_framework.authtoken import views as auth_views
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('perfis', views.PerfilViewSet)
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
-	# re_path(r'^perfis/\d+$', views.exibir, name='perfis')
-	path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar'),
-	path('convite/<int:convite_id>/aceitar', views.aceitar, name='aceitar')
+	path('', include(router.urls)),
+	path('perfil/', views.get_meu_perfil, name='perfil'),
+	path('convites/', views.get_convites, name="convites"),
+	path('convites/convidar/<int:perfil_id>', views.convidar, name='convidar'),
+	path('convites/aceitar/<int:convite_id>', views.aceitar, name='aceitar'),
+	path('login/', auth_views.obtain_auth_token, name='login')
 ]
